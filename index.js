8c568470deb0616f5aaaff59206782862427cdfb
Ogogor Akachukwu <118525443+akachi11@users.noreply.github.com>
2024-04-08 01:24:18 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require("dotenv")
+const cors = require("cors");
 
 dotenv.config()
-const cors = require('cors')
+
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 
 
-app.use(cors({
-    origin: '*'
-}));
+
 
 mongoose
     .connect(process.env.MONGO_URL)
@@ -21,9 +27,9 @@ mongoose
         console.log(err)
     })
 
-    app.use(express.json());
-    app.use('/api/users', userRoute)
-    app.use('/api/auth', authRoute)
+app.use(express.json());
+app.use('/api/users', userRoute)
+app.use('/api/auth', authRoute)
 
 app.listen(5000, () => {
     console.log("Backend is running")
